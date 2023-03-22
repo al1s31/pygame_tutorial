487c097201d65365ee3f35501a18c3c4c8120503
al1s31 <edu.berataliseydi@gmail.com>
2023-03-22 06:24:49 +0300

    Adding blocks and animations

--- main.py
+++ main.py
@@ -41,13 +41,25 @@ def load_sprite_sheet(dir1, dir2, width, height, direction = False):
 
     return all_sprites
 
+def get_block(size):
+    path = join("assets", "Terrain","Terrain.png")
+    image = pg.image.load(path).convert_alpha()
+    surface = pg.Surface((size, size),pg.SRCALPHA,32)
+    rect = pg.Rect(96,63, size, size)
+    surface.blit(image, (0,0), rect)
+    return pg.transform.scale2x(surface)
+
+
 class Player(pg.sprite.Sprite):
+
     VELOCITY = 5
     COLOR = (45, 200, 255)
     GRAVITY = 1
     SPRITES = load_sprite_sheet("MainCharacters", "NinjaFrog", 32, 32, True )
+    ANIMATION_DELAY = 3
 
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, name = None):
+        super().__init__()
         self.rect = pg.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
@@ -77,10 +89,49 @@ class Player(pg.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
     
+    def update_sprite(self):  #adding animation to character
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // 
+                         self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
+        self.update()
+    
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft = (self.rect.x, self.rect.y))
+        self.mask = pg.mask.from_surface(self.sprite)
+
     def draw(self, window):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         window.blit(self.sprite, (self.rect.x, self.rect.y))
+
+
+class Object(pg.sprite.Sprite):
+    def __init__(self, x, y, width, height, name = None):
+        super().__init__()
+        self.rect = pg.Rect(x, y, width, height)
+        self.image = pg.Surface((width, height), pg.SRCALPHA)
+        self.width = width
+        self.height = height
+        self.name = name
+
+    def draw(self,window):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Block(Object):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size, size)
+        block = get_block(size)
+        self.image.blit(block,(0,0))
+        self.mask = pg.mask.from_surface(self.image)
+
 
 def get_background(bg_name):
     image = pg.image.load(join("assets", "Background", bg_name))
@@ -94,9 +145,12 @@ def get_background(bg_name):
 
     return tiles,image
 
-def draw(window, background, bg_image, player):
+def draw(window, background, bg_image, player, objects):
     for tile in background:
         window.blit(bg_image, tile)
+
+    for object in objects:
+        object.draw(window)
 
     player.draw(window)
 
@@ -115,10 +169,13 @@ def move_handling(player):
     
 
 def main(window):
+
     clock = pg.time.Clock()
     background, bg_image = get_background("Brown.png")
+    block_size = 96
 
     player = Player(100, 100, 50, 50)
+    blocks = [Block(0,HEIGHT - block_size, block_size)]
 
     run = True
     while run:
@@ -131,7 +188,7 @@ def main(window):
         
         player.loop(FPS)
         move_handling(player)
-        draw(window, background, bg_image, player)
+        draw(window, background, bg_image, player,blocks)
         
     pg.quit()
     quit()
